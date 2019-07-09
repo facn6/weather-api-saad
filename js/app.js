248dae56fe37d3f55e4c4b678bc9a3cdeed2c820
Sam Harris <sam@jam-bc.com>
2019-07-09 21:56:45 +0300

    api call bug fix

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let getWeather = function (event) {
     fetch(url).then(function (response) {
             return response.json();
         })
-        .then(function data {
+        .then(function (data) {
             console.log(data);
         })
         .catch(function (error) {
