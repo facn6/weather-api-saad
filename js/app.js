134cc60db869d00d139e17a830db65606d49f68c
DanaEFalah <dana.e.falah@gmail.com>
2019-07-10 10:54:26 +0300

    insert google map key and url

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
-const WEATHER_KEY = "ca21dfe9667e871a5b4cfee4cabe100b";
-const ROOT_URL = "https://api.openweathermap.org/data/2.5/forecast?appid=";
 
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?appid=";
+const GOOGLE_MAPS_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCJVssKQzekpNKXWEL1VlME9wVlQUi_W9A&libraries=places&callback=initAutocomplete";
 
 let getWeather = function (event) {
     event.preventDefault();
@@ -9,7 +9,7 @@ let getWeather = function (event) {
     console.log('check city value =', event.target[0].value);
 
     var city = event.target[0].value;
-    const url = `${ROOT_URL}${keys.openWeather}&q=${city}`;
+    const url = `${WEATHER_URL}${keys.WEATHER_KEY}&q=${city}`;
 
     fetch(url).then(function (response) {
             return response.json();
